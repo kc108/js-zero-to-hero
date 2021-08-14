f2f3472557b9f074686f1270bd12a9de42ad1995
Kim Carpico <kcarpico@gmail.com>
2021-08-14 11:14:28 -0700

    dom manipulation practice

---
+++ 05-Guess-My-NUmber/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+// this line changes the text content
+document.querySelector('.message').textContent = 'Correct Number! 🙌🏽';
+
+// shows changes on screen
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
