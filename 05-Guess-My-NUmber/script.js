74a156fe0ef025a99a0dad903048fb46f1ea4f02
Kim Carpico <kcarpico@gmail.com>
2021-08-14 11:59:10 -0700

    cont practice with DOM manipulation

--- 05-Guess-My-NUmber/script.js
+++ 05-Guess-My-NUmber/script.js
@@ -1,19 +1,25 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-// this line changes the text content
-document.querySelector('.message').textContent = 'Correct Number! 🙌🏽';
+// console.log(document.querySelector('.message').textContent);
+// // this line changes the text content
+// document.querySelector('.message').textContent = 'Correct Number! 🙌🏽';
 
-// shows changes on screen
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// // shows changes on screen
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-// writes 23 in user input box
-document.querySelector('.guess').value = 23;
-// to get the value we use the value property
-console.log(document.querySelector('.guess').value);
+// // writes 23 in user input box
+// document.querySelector('.guess').value = 23;
+// // to get the value we use the value property
+// console.log(document.querySelector('.guess').value);
 
 // EVENT LISTENER
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// the score is the state
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 // CHECK to make sure this appears in console using (cmd + i)
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,5 +28,18 @@ document.querySelector('.check').addEventListener('click', function () {
   // 1st input is to assume there is NO input value
   if (!guess) {
     document.querySelector('.message').textContent = '👹 No number!';
+    // equal to secretNumber
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🙌🏽 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+    // decrease the score
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
+    // decrease the score
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
