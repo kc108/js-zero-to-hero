d7f719d5307dbe48a92bcec093730cd2fed70922
Kim Carpico <kcarpico@gmail.com>
2021-08-14 18:07:46 -0700

    js modal practice continued

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,19 +5,18 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    // REMOVE() does not use the class CSS property with a '.'
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
-// FUNCTION USED TO CLOSE THE MODAL
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
