824352a57d7f1fce6b7d2f8c521cf8eb6bc86127
Kim Carpico <kcarpico@gmail.com>
2021-08-14 19:29:15 -0700

    finished practicing modal js practice

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -20,3 +20,17 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// We are not using the 'event' at this time, but we will use later
+// this will provide info about what key is pressed
+document.addEventListener('keydown', function (e) {
+  // console.log('A key was pressed');
+  // LOGGED to see what is in the 'e'.
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    // if the modal class does NOT contain the hidden class
+    // if this.. and if this (latter first then the first one)
+    closeModal();
+  }
+});
