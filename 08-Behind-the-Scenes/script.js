00e58ea08465aac1b99ac49e3d4f72e1bbfe8cfa
Kim Carpico <kcarpico@gmail.com>
2021-08-16 14:42:28 -0700

    practice and reviewing how js works under the hood

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -5,13 +5,26 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    let output = `You are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
       const str = `Oh, and you're a millenial, ${firstName}.`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT!';
     }
+
+    console.log(millenial);
+    console.log(output);
   }
 
   printAge();
