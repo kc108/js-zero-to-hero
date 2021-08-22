7926b6b8a73bbdcebd3b480ebdae19b68aa66c87
Kim Carpico <kcarpico@gmail.com>
2021-08-21 17:30:33 -0700

    passing arguments: value vs. reference

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -30,3 +30,47 @@
 ///////////////////////////////////////////////////////
 // HOW PASSING ARGUMENTS WORKS: VALUES VS. REFERENCE
 ///////////////////////////////////////////////////////
+// APPLIED TO FUNCTIONS
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24564567856,
+};
+
+const checkIn = function (flightNum, passenger) {
+  // DO NOT reassign the parameters in the function, this is just to show what happens
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24564567856) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+///////////////////////////////////////////////////////
+// IS THE SAME AS DOING
+///////////////////////////////////////////////////////
+const flightNum = flight;
+const passenger = jonas;
+
+// ANOTHER EXAMPLE OF QUICK FUNCTION
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10_000_000_000);
+};
+
+// THIS SHOWS 'WRONG PASSPORT' because it has the same passport as the above function
+newPassport(jonas);
+checkIn(flight, jonas);
+
+// passing by reference: JS DOES NOT DO THIS!
+// passing by value: JS ONLY DOES THIS.
+
+///////////////////////////////////////////////////////
+//
+///////////////////////////////////////////////////////
